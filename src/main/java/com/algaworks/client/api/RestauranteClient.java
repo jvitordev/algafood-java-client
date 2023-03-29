9553a99a720a4ce0b75264650f0b9156f1166da4
Joao Vitor <joaovitorti07@gmail.com>
2023-03-29 12:37:44 -0300

    Aula 16.11. Tratando respostas com código de erro no client Java

--- src/main/java/com/algaworks/client/api/RestauranteClient.java
+++ src/main/java/com/algaworks/client/api/RestauranteClient.java
@@ -4,6 +4,7 @@ import java.net.URI;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.web.client.RestClientResponseException;
 import org.springframework.web.client.RestTemplate;
 
 import com.algaworks.client.model.RestauranteResumoModel;
@@ -19,11 +20,16 @@ public class RestauranteClient {
 	private String url;
 	
 	public List<RestauranteResumoModel> listar() {
-		URI resourceUri = URI.create(url + RESOURCE_PATH);
-		
-		RestauranteResumoModel[] restaurantes = restTemplate
-				.getForObject(resourceUri, RestauranteResumoModel[].class);
-		
-		return Arrays.asList(restaurantes);
+		try {
+			URI resourceUri = URI.create(url + RESOURCE_PATH);
+			
+			RestauranteResumoModel[] restaurantes = restTemplate
+					.getForObject(resourceUri, RestauranteResumoModel[].class);
+			
+			return Arrays.asList(restaurantes);
+		} catch (RestClientResponseException e) {
+			
+			throw new ClientApiException(e.getMessage(), e);
+		}
 	}
 }
